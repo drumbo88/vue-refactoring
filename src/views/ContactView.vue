774bb36a8aaca9cba5f1d634fa81f0bd418fa5de
Darío Rumbo <dario.rumbo@gmail.com>
2023-09-05 09:23:45 -0300

    Using Composables for refactoring programmatic parts

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,9 +1,13 @@
 <template>
     <MainLayout>
-        <h1>Contact Section</h1>
+        <h1>Contact Section (coordinates: ({{ x }}, {{ y }}))</h1>
     </MainLayout>
 </template>
 
 <script lang="ts" setup>
 import MainLayout from '@/layouts/MainLayout.vue'
+
+import { useMouse } from '@/composables/useMouse'
+
+const { x, y } = useMouse()
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,14 @@
     <MainLayout>
         <h1>My Home Content</h1>
         <template #aux>
-            <h1>Aux content</h1>
+            <h1>X position is {{ x }}, Y position is: {{ y }}</h1>
         </template>
     </MainLayout>
 </template>
 
 <script lang="ts" setup>
 import MainLayout from '@/layouts/MainLayout.vue'
-import { ref, onMounted, onUnmounted } from 'vue'
+import { useMouse } from '@/composables/useMouse'
 
-const x = ref(0)
-const y = ref(0)
-
-function update(event: MouseEvent) {
-    x.value = event.pageX
-    y.value = event.pageY
-}
-onMounted(() => window.addEventListener('mousemove', update))
-onUnmounted(() => window.removeEventListener('mousemove', update))
+const { x, y } = useMouse()
 </script>
